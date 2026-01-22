f9c1cc59318c54eef74c5dc20a32d7d0abbd76ac
Dan Hariton <dhariton@griddynamics.com>
2026-01-22 12:26:31 +0200

    update aria label for search submit button

--- src/components/Header/components/SearchBar/SearchBar.tsx
+++ src/components/Header/components/SearchBar/SearchBar.tsx
@@ -5,7 +5,7 @@ export function SearchBar() {
   return (
     <div className={styles.searchBar}>
       <input type="search" placeholder="Search this site" className={styles.input} />
-      <button type="submit" name="submit icon button" className={styles["search-icon-button"]}>
+      <button type="submit" aria-label="submit search" className={styles["search-icon-button"]}>
         <CiSearch className={styles.icon} />
       </button>
     </div>
